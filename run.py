c002bf68682206cfdc43637fe1d1b048cd36a0cf
vicgoodall <vic.goodall@icloud.com>
2023-02-04 21:42:33 +0000

    added user request loop so they can retry until correct data is entered

--- run.py
+++ run.py
@@ -16,14 +16,19 @@ def get_sales_data():
     """
     Get sales figures from user
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+       print("Please enter sales data from the last market.")
+       print("Data should be six numbers, separated by commas.")
+       print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
+       data_str = input("Enter your data here: ")
     
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+       sales_data = data_str.split(",")
+       validate_data(sales_data)
+
+       if validate_data(sales_data):
+         print("Data is valid!")
+         break
 
 def validate_data(values):
     """
@@ -31,7 +36,7 @@ def validate_data(values):
     Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values
     """
-    print(values)
+    
     try:
         [int(value) for value in values]
         if len(values) != 6:
@@ -40,7 +45,9 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again. \n")
+        return False
 
+    return True
 
 
 get_sales_data()
